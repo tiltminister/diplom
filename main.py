927fa89c7678642b592ce959d0bd12bca29d2dc9
tiltminister <advisback@gmail.com>
2024-05-27 14:57:23 +0300

    123

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from scipy.integrate import quad
 import math
+import numpy as np
+import warnings
 
+warnings.simplefilter("ignore")
 
 def ctg(x):
-    return 1/math.tan(x)
+    return 1 / math.tan(x)
+
 
 def integrate_function(func, a, b):
     """
@@ -15,10 +19,34 @@ def integrate_function(func, a, b):
     :return: Значение определенного интеграла или сообщение об ошибке.
     """
     try:
+        # Проверка на наличие вертикальных асимптот
+        if has_poles(func, a, b):
+            return "Функция имеет вертикальные асимптоты на интервале интегрирования."
+
         integral, error = quad(func, a, b)
         return integral
     except Exception as e:
         return str(e)
+
+
+def has_poles(func, a, b, num_points=1000):
+    """
+    Проверяет, есть ли у функции вертикальные асимптоты на интервале [a, b].
+
+    :param func: Функция для проверки.
+    :param a: Нижний предел интервала.
+    :param b: Верхний предел интервала.
+    :param num_points: Количество точек для проверки.
+    :return: True, если асимптоты обнаружены, иначе False.
+    """
+    x_vals = np.linspace(a, b, num_points)
+    for x in x_vals:
+        try:
+            if np.isinf(func(x)) or np.isnan(func(x)):
+                return True
+        except ZeroDivisionError:
+            return True
+    return False
 
 
 def input_function():
@@ -32,10 +60,10 @@ def input_function():
             user_input = input("Введите функцию от x (например, sin(x), log(x), exp(x), x**2 + 2*sin(x)):\n")
             # Добавляем 'math.' перед каждой функцией
             user_input = user_input.replace('sin', 'math.sin').replace('cos', 'math.cos').replace('tan', 'math.tan')
-            user_input = user_input.replace('log', 'math.log').replace('exp', 'math.exp').replace('sqrt', 'math.sqrt')
+            user_input = user_input.replace('log', 'math.log').replace('ln', 'math.log').replace('exp', 'math.exp').replace('sqrt', 'math.sqrt')
             func = eval("lambda x: " + user_input)
             # Пробное вычисление для проверки корректности функции
-            func(1)
+            func(10)
             return func
         except Exception as e:
             print(f"Ошибка ввода функции: {e}. Пожалуйста, попробуйте снова.")
@@ -63,6 +91,6 @@ a, b = input_limits()
 # Пытаемся вычислить интеграл и выводим результат или сообщение об ошибке
 result = integrate_function(func, a, b)
 if isinstance(result, float):
-    print(f"Интеграл функции от {a} до {b} равен {round(result,2)}")
+    print(f"Интеграл функции от {a} до {b} равен {round(result, 2)}")
 else:
     print(f"Ошибка при вычислении интеграла: {result}")
